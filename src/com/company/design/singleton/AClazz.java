82b3e8c5831ab0bf444e247b9f2b28bfb6cadb89
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-07-17 12:25:57 +0900

    패스트캠퍼스 강의 시작

---
+++ src/com/company/design/singleton/AClazz.java
@@ -0,0 +1,14 @@
+package com.company.design.singleton;
+
+public class AClazz {
+
+    private SocketClient socketClient;
+
+    public AClazz() {
+        this.socketClient = socketClient.getInstance();
+    }
+
+    public SocketClient getSocketClient() {
+        return socketClient;
+    }
+}
