e0ec11b850c2bfbcd93dad660ab45b6cdd0cd14e
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-07-18 10:40:17 +0900

    연습한거 지우기

--- src/com/company/design/practice/Practice1.java
+++
@@ -1,7 +0,0 @@
-package com.company.design.practice;
-
-public class Practice1 {
-    public static void main(String[] args) {
-        System.out.println("테스트 잘되냐");
-    }
-}
--- src/com/company/design/practice/Practice2.java
+++
@@ -1,7 +0,0 @@
-package com.company.design.practice;
-
-public class Practice2 {
-    public static void main(String[] args) {
-        System.out.println("테스트 잘 되어라");
-    }
-}
