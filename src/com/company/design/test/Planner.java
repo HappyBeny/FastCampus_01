55e3badaafe94f03fdedb255e7b3236373a8e0db
Beny Bang <qkdehdgur3@gmail.com>
2024-07-22 21:13:24 +0900

    플래너 테스트-1

--- src/com/company/design/test/Planner.java
+++ src/com/company/design/test/Planner.java
@@ -1,2 +1,25 @@
-package com.company.design.test;public class Planner {
+package com.company.design.test;
+
+import java.util.Scanner;
+
+public class Planner {
+    Scanner sc = new Scanner(System.in);
+    private int n = 5;
+    private String name;
+    private int age;
+    private String purpose;
+    private String[] mon = new String[n];
+
+    private Planner(){};
+
+    public Planner (String name, int age, String purpose) {
+        this.name = name;
+        this.age = age;
+        this.purpose = purpose;
+    }
+
+
+    public void addSchedule(){
+
+    }
 }
--- src/com/company/design/test/PlannerTestMain.java
+++ src/com/company/design/test/PlannerTestMain.java
@@ -1,2 +1,7 @@
-package com.company.design.test;public class PlannerTest {
+package com.company.design.test;
+
+public class PlannerTestMain {
+    public static void main(String[] args) {
+        Planner P1 = new Planner("방동혁", 32, "테스트용");
+    }
 }
